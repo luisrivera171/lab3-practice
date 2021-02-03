a9e3bce9ea905d6234750522a0056e387a5df44f
luisrivera171 <77513171+luisrivera171@users.noreply.github.com>
2021-02-03 17:33:39 -0400

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
+/* This is a test for creating a file in folder and push it to 
+github */
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # lab3-practice
 This is a test.
+
+This is a test for pushing.
