149c0def989397c5a35881aad50231df3ea503b5
luisrivera171 <luis.rivera171@upr.edu>
2021-02-03 22:25:04 -0400

    Añadí el cin del name

--- Hello.cpp
+++ Hello.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 int main(){
-    cout << "Hello World!" << endl;
+    string name;
+    cout << "What is your name?" << endl;
+    cin >> name;
+    cout << "Hello " << name << endl;
     return 0;
 }
 
