2f934a160f826fb70a9d6c168598bc2f62db9bfc
UcppRperez <ricardo.perez21@upr.edu>
2021-02-04 21:27:30 -0500

    I Shared Something

---
+++ Sharing.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    cout << "Attempt..." << endl;
+    int tvalue = 1 + 1;
+    cout << tvalue << " is better than 1" << endl;
+
+    return 0;
+}
